fb09b382a57266bfaaab9b0346209eaa80f60404
nader8 <nader@getspokn.com>
2020-03-28 22:48:01 +0200

    log modal

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,14 +1,15 @@
-import React, { useEffect } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
 import { connect } from 'react-redux';
 import { getAllMovies } from '../redux/actions/movies';
 import ItemCard from './ItemCard';
+import LogModal from './LogModal';
 
 const Movies = ({ getAllMovies, movies }) => {
   useEffect(() => {
     getAllMovies();
   }, [getAllMovies]);
-
+  const [visible, setModal] = useState(false);
   const renderMovies = () => {
     return (
       movies &&
@@ -22,7 +23,20 @@ const Movies = ({ getAllMovies, movies }) => {
       ))
     );
   };
-  return <View style={styles.container}>{renderMovies()}</View>;
+  return (
+    <>
+      <View style={styles.container}>
+        <LogModal visible={visible} setModal={setModal} />
+        <View style={styles.header}>
+          <Text style={{ color: '#445566', fontSize: 20, fontWeight: '900' }}>
+            I Watched ...
+          </Text>
+          <Button onPress={() => setModal(true)} title="Log" color="#55B02C" />
+        </View>
+        {renderMovies()}
+      </View>
+    </>
+  );
 };
 
 const mapStateToProps = (state) => ({
@@ -38,5 +52,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#1D242B',
     paddingTop: 20,
+  },
+  header: {
+    height: 40,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingRight: 20,
+    paddingLeft: 20,
+    marginBottom: 20,
   },
 });
